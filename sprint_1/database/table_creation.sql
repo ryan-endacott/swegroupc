a795127ef0cc593effacc652aeec81ab999739e7
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-05 17:24:30 -0500

    Added the DDL for the Student table

---
+++ sprint_1/database/table_creation.sql
@@ -0,0 +1,14 @@
+﻿-- Create the Student table.
+
+CREATE TABLE sprint_1.student
+(
+  pawprint character varying(10) NOT NULL,
+  "firstName" character varying(50) NOT NULL,
+  "lastName" character varying(50) NOT NULL,
+  CONSTRAINT pawprint_primary_key PRIMARY KEY (pawprint)
+);
+
+ALTER TABLE sprint_1.student
+  ADD CONSTRAINT pawprint_unique UNIQUE (pawprint);
+
+-- Create the TA table.
