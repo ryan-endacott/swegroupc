2654ea55adb63a7b2170ca1b90f2f6d428f8d323
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-05 17:55:13 -0500

    Added logic to create the TA table

--- sprint_1/database/table_creation.sql
+++ sprint_1/database/table_creation.sql
@@ -13,3 +13,15 @@ CREATE TABLE swe.student
 ALTER TABLE swe.student
   ADD CONSTRAINT pawprint_primary_key PRIMARY KEY (pawprint);
 
+-- Create the TA table.
+
+DROP TABLE IF EXISTS swe.ta;
+CREATE TABLE swe.ta
+(
+  pawprint character varying(10) NOT NULL
+);
+
+ALTER TABLE swe.ta
+  ADD CONSTRAINT pawprint_foreign_key
+        FOREIGN KEY (pawprint) REFERENCES swe.student(pawprint);
+
