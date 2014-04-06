23ca1540507628ab423c659df4a001099cae5176
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-05 23:57:51 -0500

    Finished up the DB creation logic

--- sprint_1/database/table_creation.sql
+++ sprint_1/database/table_creation.sql
@@ -12,16 +12,17 @@ CREATE TABLE swe.student
 
 -- Create the TA table.
 
-DROP TABLE IF EXISTS swe.ta;
+DROP TABLE IF EXISTS swe.ta CASCADE;
 CREATE TABLE swe.ta
 (
-  pawprint character varying(10) NOT NULL REFERENCES swe.student(pawprint)
+  pawprint character varying(10) REFERENCES swe.student(pawprint),
+  PRIMARY KEY (pawprint)
 );
 
 -- Create the Instructor table.
 
-DROP TABLE IF EXISTS swe.instructor;
-CREATE table swe.instructor
+DROP TABLE IF EXISTS swe.instructor CASCADE;
+CREATE TABLE swe.instructor
 (
   id serial PRIMARY KEY,
   title character varying(20),
@@ -31,8 +32,8 @@ CREATE table swe.instructor
 
 -- Create the Course table.
 
-DROP TABLE IF EXISTS swe.course;
-CREATE table swe.course
+DROP TABLE IF EXISTS swe.course CASCADE;
+CREATE TABLE swe.course
 (
   id serial PRIMARY KEY,
   name character varying(255) NOT NULL,
@@ -41,10 +42,64 @@ CREATE table swe.course
 
 -- Create the Section table.
 
-DROP TABLE IF EXISTS swe.section;
-CREATE table swe.section
+DROP TABLE IF EXISTS swe.section CASCADE;
+CREATE TABLE swe.section
 (
   id serial PRIMARY KEY,
-  name character varying (255) NOT NULL,
+  name character varying(255) NOT NULL,
   course_id serial REFERENCES swe.course(id)
+);
+
+-- Create the Student Course table.
+
+DROP TABLE IF EXISTS swe.student_course;
+CREATE TABLE swe.student_course
+(
+  pawprint character varying(10) REFERENCES swe.student(pawprint),
+  course_id serial REFERENCES swe.course(id),
+  PRIMARY KEY (pawprint, course_id)
+);
+
+-- Create the Student Section table
+
+DROP TABLE IF EXISTS swe.student_section;
+CREATE TABLE swe.student_section
+(
+  pawprint character varying(10) REFERENCES swe.student(pawprint),
+  section_id serial REFERENCES swe.section(id),
+  PRIMARY KEY (pawprint, section_id)
+);
+
+-- Create the TA Course table.
+
+DROP TABLE IF EXISTS swe.ta_course;
+CREATE TABLE swe.ta_course
+(
+  pawprint character varying(10) REFERENCES swe.ta(pawprint),
+  course_id serial REFERENCES swe.course(id),
+  PRIMARY KEY (pawprint, course_id)
+);
+
+-- Create the TA Section table.
+
+DROP TABLE IF EXISTS swe.ta_section;
+CREATE TABLE swe.ta_section
+(
+  pawprint character varying(10) REFERENCES swe.ta(pawprint),
+  section_id serial REFERENCES swe.section(id),
+  PRIMARY KEY (pawprint, section_id)
+);
+
+-- Create the Submission table.
+
+DROP TABLE IF EXISTS swe.submission;
+CREATE TABLE swe.submission
+(
+  receipt character varying(255) PRIMARY KEY,
+  section_id serial REFERENCES swe.section(id),
+  pawprint character varying(10) REFERENCES swe.student(pawprint),
+  filename character varying(255) NOT NULL,
+  file_contents bytea NOT NULL,
+  submission_date timestamp NOT NULL,
+  ip timestamp NOT NULL
 );
