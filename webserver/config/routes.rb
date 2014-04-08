96dfa3594f77e84f6f217dbf420ca4226676b30e
Ryan Endacott <rzeg24@gmail.com>
2014-04-07 19:12:29 -0500

    added submission routes

--- webserver/config/routes.rb
+++ webserver/config/routes.rb
@@ -2,4 +2,6 @@ Webserver::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  get 'submission' => 'submissions#new'
+  post 'submission' => 'submissions#create'
 end
