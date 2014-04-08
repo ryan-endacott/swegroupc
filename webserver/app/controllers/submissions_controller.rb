0395f5e1780759c1941314118a9f32cf1123707a
Ryan Endacott <rzeg24@gmail.com>
2014-04-07 20:31:39 -0500

    got file upload working

--- webserver/app/controllers/submissions_controller.rb
+++ webserver/app/controllers/submissions_controller.rb
@@ -69,6 +69,6 @@ class SubmissionsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def submission_params
-      params.require(:submission).permit(:receipt, :user_id, :ip_address, :filename, :content_type, :file_contents)
+      params.require(:submission).permit(:receipt, :user_id, :ip_address, :file)
     end
 end
--- webserver/app/models/submission.rb
+++ webserver/app/models/submission.rb
@@ -1,3 +1,22 @@
 class Submission < ActiveRecord::Base
   belongs_to :user
+  before_save :save_file
+
+  def initialize(params = {})
+    @file = params.delete(:file)
+    super
+  end
+
+  private
+
+    def save_file
+      self.filename = sanitize_filename(@file.original_filename)
+      self.content_type = @file.content_type
+      self.file_contents = @file.read
+    end
+
+    def sanitize_filename(filename)
+      # Just get filename in case of IE
+      File.basename(filename)
+    end
 end
