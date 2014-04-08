88dc300927a3b69b11d293014a2d355a4f952072
Ryan Endacott <rzeg24@gmail.com>
2014-04-07 21:10:38 -0500

    now correctly validates file size

--- webserver/app/models/submission.rb
+++ webserver/app/models/submission.rb
@@ -2,12 +2,21 @@ class Submission < ActiveRecord::Base
   belongs_to :user
   before_save :save_file
 
+  validate :file_size_under_five_mb
+
   def initialize(params = {})
     @file = params.delete(:file)
     super
   end
 
   private
+
+    def file_size_under_five_mb
+      # 1048576 is the number of bytes in a megabyte
+      if (@file.size.to_f / 1048576) > 5
+        errors.add(:file, 'File size cannot be over 5 MB')
+      end
+    end
 
     def save_file
       self.filename = sanitize_filename(@file.original_filename)
