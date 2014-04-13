3400d1465f6f27765124abf226df20d9463b1f12
hcfxd_111816 <hcfxd@babbage.cs.missouri.edu>
2014-04-13 15:52:18 -0500

    change pg_query to pg_p and pg_e

--- sprint2_collection/parameters.php
+++ sprint2_collection/parameters.php
@@ -1,9 +1,11 @@
 <?php
     $dbconn = pg_connect("host='localhost' dbname='dbname' user='user' password='password' port='port'") OR DIE ('Could not connect: ' . pg_last_error());
-    $query = "SELECT file_contents
+    $query = 'SELECT file_contents
                 FROM swe.submission
-                WHERE id = " . $section_id;
-    $result = pg_query($dbconn, $query);
+                WHERE id = $1';
+	$stmt = pg_prepare($conn, "parameters", $query);
+	$result = pg_execute($conn, "parameters", array($section_id));
+	
     while($row = pg_fetch_array($result, null, PGSQL_ASSOC))
 	{
         $escapeBytea = $row['file_contents'];
