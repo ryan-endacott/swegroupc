170fe6d28320842a90ef3011835a7a2489d7b1e5
hcfxd_111816 <hcfxd@babbage.cs.missouri.edu>
2014-04-13 15:09:59 -0500

    upload parameters

---
+++ sprint2_collection/parameters.php
@@ -0,0 +1,12 @@
+<?php
+    $dbconn = pg_connect("host='localhost' dbname='dbname' user='user' password='password' port='port'") OR DIE ('Could not connect: ' . pg_last_error());
+    $query = "SELECT file_contents
+                FROM swe.submission
+                WHERE id = " . $section_id;
+    $result = pg_query($dbconn, $query);
+    while($row = pg_fetch_array($result, null, PGSQL_ASSOC)){
+        $escapeBytea = $row['file_contents'];
+        $fileContents = pg_unescape_bytea($escapeBytea); 
+        file_put_contents($fileName, $fileContents); 
+}
+?>
