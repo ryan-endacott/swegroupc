271f0a81d2f321bf2d6395d7e16e201cfb99348b
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-14 13:27:22 -0500

    Added logic for adding a TA
    Is dependent on the student table.

--- database/table_manipulation.sql
+++ database/table_manipulation.sql
@@ -1,5 +1,11 @@
 -- Add a student.
 
 INSERT INTO swe.student
-VALUES ('{pawprint}', '{first_name}', '{last_name}');
+    VALUES ('{pawprint}', '{first_name}', '{last_name}');
 
+-- Add a TA.
+
+INSERT INTO swe.ta
+    VALUES ('{pawprint}');
+
+INSERT
