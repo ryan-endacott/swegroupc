16c0fce68d98a6c2f06bb6339c021baeb3b8a68b
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-14 22:21:35 -0500

    And... the belongs_to

--- webserver/db/migrate/20140414205452_create_student_sections.rb
+++ webserver/db/migrate/20140414205452_create_student_sections.rb
@@ -1,8 +1,8 @@
 class CreateStudentSections < ActiveRecord::Migration
   def change
     create_table :student_sections, :id => false do |t|
-      t.references :student
-      t.references :section
+      t.belongs_to :student
+      t.belongs_to :section
     end
 
     add_index :student_sections, [:student_id, :section_id], :unique => true
