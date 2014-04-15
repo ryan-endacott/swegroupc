aa952863ca4d106887bdadb3e716bb81bd7ee145
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-14 22:16:00 -0500

    Added a many-to-many student_sectionstable

--- webserver/app/models/student.rb
+++ webserver/app/models/student.rb
@@ -1,3 +1,4 @@
 class Student < ActiveRecord::Base
   has_and_belongs_to_many :courses
+  has_and_belongs_to_many :sections
 end
---
+++ webserver/db/migrate/20140414205452_create_student_sections.rb
@@ -0,0 +1,10 @@
+class CreateStudentSections < ActiveRecord::Migration
+  def change
+    create_table :student_sections do |t|
+      t.references :student
+      t.references :section
+    end
+
+    add_index :student_sections, [:student_id, :section_id], :unique => true
+  end
+end
