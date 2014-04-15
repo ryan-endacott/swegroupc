335a8cbd98f0417a8374d54a9148b0b702b2d91c
Duncan Hill <dahr47@mail.missouri.edu>
2014-04-14 22:28:53 -0500

    Ran a migration

--- webserver/db/schema.rb
+++ webserver/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414204631) do
+ActiveRecord::Schema.define(version: 20140415032304) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20140414204631) do
 
   add_index "student_courses", ["student_id", "course_id"], name: "index_student_courses_on_student_id_and_course_id", unique: true
 
+  create_table "student_sections", id: false, force: true do |t|
+    t.integer "student_id"
+    t.integer "section_id"
+  end
+
+  add_index "student_sections", ["student_id", "section_id"], name: "index_student_sections_on_student_id_and_section_id", unique: true
+
   create_table "students", force: true do |t|
     t.string   "pawprint"
     t.string   "first_name"
@@ -76,6 +83,20 @@ ActiveRecord::Schema.define(version: 20140414204631) do
   end
 
   add_index "ta", ["student_id"], name: "index_ta_on_student_id"
+
+  create_table "ta_courses", id: false, force: true do |t|
+    t.integer "ta_id"
+    t.integer "course_id"
+  end
+
+  add_index "ta_courses", ["ta_id", "course_id"], name: "index_ta_courses_on_ta_id_and_course_id", unique: true
+
+  create_table "ta_sections", id: false, force: true do |t|
+    t.integer "ta_id"
+    t.integer "section_id"
+  end
+
+  add_index "ta_sections", ["ta_id", "section_id"], name: "index_ta_sections_on_ta_id_and_section_id", unique: true
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
