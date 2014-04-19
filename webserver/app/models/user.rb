c744648311942c2260cacc648dbffbb1f134cdd4
Ryan Endacott <rzeg24@gmail.com>
2014-04-19 17:58:33 -0500

    added student? ta? and instructor? methods to user

--- webserver/app/models/user.rb
+++ webserver/app/models/user.rb
@@ -2,4 +2,16 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :rememberable, :trackable
+
+  def instructor?
+    return self.class == Instructor
+  end
+
+  def student?
+    return self.class == Student
+  end
+
+  def ta?
+    return self.class == Ta
+  end
 end
