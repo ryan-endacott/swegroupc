b1ccb1d9a6accc1eac18a060ed7f5e6f826163c5
Ryan Endacott <rzeg24@gmail.com>
2014-04-19 23:19:22 -0500

    scaffolded courses

--- webserver/config/routes.rb
+++ webserver/config/routes.rb
@@ -2,4 +2,5 @@ Webserver::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :submissions
+  resources :courses
 end
