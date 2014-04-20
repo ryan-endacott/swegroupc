3ec453f0313974a126553cee3ebbf073836d6aaa
Ryan Endacott <rzeg24@gmail.com>
2014-04-20 13:38:22 -0500

    made courses and assignments and submissions dependent destroy

--- webserver/app/models/assignment.rb
+++ webserver/app/models/assignment.rb
@@ -1,5 +1,5 @@
 class Assignment < ActiveRecord::Base
   belongs_to :course
-  has_many :submissions
+  has_many :submissions, dependent: :destroy
   validates :course, :name, :due_date, presence: true
 end
--- webserver/app/models/course.rb
+++ webserver/app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
   belongs_to :instructor
-  has_many :assignments
+  has_many :assignments, dependent: :destroy
 
   validates :instructor, :name, presence: true
 end
