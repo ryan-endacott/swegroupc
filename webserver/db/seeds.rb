451df44a63b3d83b27c6aa2d3de5aa5858f2d7e4
Ryan Endacott <rzeg24@gmail.com>
2014-04-19 22:40:23 -0500

    made it so we can seed professors now!

--- webserver/db/seeds.rb
+++ webserver/db/seeds.rb
@@ -1,22 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Environment variables (ENV['...']) can be set in the file config/application.yml.
-# See http://railsapps.github.io/rails-environment-variables.html
-#puts 'DEFAULT USERS'
-#user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-#puts 'user: ' << user.name
 
-# TODO: Confirm that this is conditional on production.
-puts 'DEFAULT STUDENTS'
+# Populates professor list from seed_data/professors.txt
+puts 'Populating professor list...'
 
-student = Student.new :first_name => 'Duncan',
-                      :last_name => 'Hill',
-                      :pawprint => 'dahr47'
-student.save
-puts 'Student: ' << student.first_name << ' ' << student.last_name
+File.readlines("#{Rails.root}/db/seed_data/professors.txt").each do |line|
+
+  line.strip! # trim whitespace
+
+  next if line.chars.first == '#' # Skip if line is a comment
+
+  # Otherwise, add professor to the database if they are not in it already
+  Instructor.where(pawprint: line, email: "#{line}@missouri.edu").first_or_create
+
+end
+
+puts 'Done populating professor list'
 
