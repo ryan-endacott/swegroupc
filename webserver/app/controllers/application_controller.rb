45a0fe19def5e03b514b023fd378313bec6f354f
Ryan Endacott <rzeg24@gmail.com>
2014-04-19 23:19:14 -0500

    helpers for instructors only, tas only, students only

--- webserver/app/controllers/application_controller.rb
+++ webserver/app/controllers/application_controller.rb
@@ -2,4 +2,28 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+    def instructors_only!
+      unless user_signed_in? && current_user.instructor?
+        flash[:error] = 'You must be an instructor to access that page.'
+        redirect_to root_path
+      end
+    end
+
+    def students_only!
+      unless user_signed_in? && current_user.student?
+        flash[:error] = 'You must be a student to access that page.'
+        redirect_to root_path
+      end
+    end
+
+    def tas_only!
+      unless user_signed_in? && current_user.ta?
+        flash[:error] = 'You must be a teaching assistant to access that page.'
+        redirect_to root_path
+      end
+    end
+
 end
