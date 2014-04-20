629fe896d919452bc5303c022342d73d6bdece54
Ryan Endacott <rzeg24@gmail.com>
2014-04-20 17:25:19 -0500

    sections get deleted when a course is deleted

--- webserver/app/models/course.rb
+++ webserver/app/models/course.rb
@@ -1,7 +1,7 @@
 class Course < ActiveRecord::Base
   belongs_to :instructor
   has_many :assignments, dependent: :destroy
-  has_many :sections
+  has_many :sections, dependent: :destroy
   has_and_belongs_to_many :tas
 
   validates :instructor, :name, presence: true
