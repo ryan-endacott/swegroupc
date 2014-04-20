2dd9f30f36f93036c4ef6456628f6e086819a3e8
Ryan Endacott <rzeg24@gmail.com>
2014-04-20 02:05:43 -0500

    made active navbar brand

--- webserver/app/helpers/application_helper.rb
+++ webserver/app/helpers/application_helper.rb
@@ -12,4 +12,12 @@ module ApplicationHelper
     html.html_safe
   end
 
+  def nav_link(link_text, link_path, options = nil)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path, options
+    end
+  end
+
 end
