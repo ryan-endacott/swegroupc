a093f41d2933cd278d777e3d57351c0cef89fa02
Ryan Endacott <rzeg24@gmail.com>
2014-04-20 13:30:33 -0500

    removed has and belongs to many students on course and section

--- webserver/app/models/course.rb
+++ webserver/app/models/course.rb
@@ -1,6 +1,5 @@
 class Course < ActiveRecord::Base
   belongs_to :instructor
-  has_and_belongs_to_many :students
   has_many :assignments
 
   validates :instructor, :name, presence: true
--- webserver/app/models/section.rb
+++ webserver/app/models/section.rb
@@ -1,4 +1,3 @@
 class Section < ActiveRecord::Base
   belongs_to :course
-  has_and_belongs_to_many :students
 end
