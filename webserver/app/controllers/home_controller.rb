00b44409eec938858735cc2081e6ff0ef0d3d44c
Ryan Endacott <rzeg24@gmail.com>
2014-04-20 01:42:36 -0500

    emptied home controller, no more redirect at root

--- webserver/app/controllers/home_controller.rb
+++ webserver/app/controllers/home_controller.rb
@@ -1,9 +1,2 @@
 class HomeController < ApplicationController
-  def index
-    if user_signed_in?
-      redirect_to submissions_path
-    else
-      redirect_to new_user_session_path
-    end
-  end
 end
