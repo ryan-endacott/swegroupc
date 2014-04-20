adfebda2964f670aaae97a66be6c0e93be0f838a
Ryan Endacott <rzeg24@gmail.com>
2014-04-19 22:05:53 -0500

    redirect to sign in or submissions path on first load

--- webserver/app/controllers/home_controller.rb
+++ webserver/app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+    if user_signed_in?
+      redirect_to submissions_path
+    else
+      redirect_to new_user_session_path
+    end
+  end
 end
