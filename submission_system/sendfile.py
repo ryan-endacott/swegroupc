84d31eb0f51f4c3e0a52005b0c7f7cea8e58c6fa
bx3g3 <bx3g3@mail.missouri.edu>
2014-04-19 19:18:01 -0500

    update the sendfile part

--- submission_system/sendfile.py
+++ submission_system/sendfile.py
@@ -5,11 +5,20 @@ import sys
 """ 
 read in the filename, the first three is submit course section
 """
-i=4
+i=0
+filefolder=[]
+while(i<3):
+	filefolder.append(sys.argv[i])
+	i++
+
+
+i=3
 filename=[]
 while(i<len(sys.argv)):
 	filename.append(sys.argv[i])
 	i=i+1
+
+
 
 i=0
 while(i<len(filename)):
