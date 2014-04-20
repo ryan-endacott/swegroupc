f74367f9e64107fbb6ecbaaf5c410fdee7078168
Ryan Endacott <rzeg24@gmail.com>
2014-04-19 23:22:55 -0500

    validate presence of course name

--- webserver/app/models/course.rb
+++ webserver/app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ActiveRecord::Base
   belongs_to :instructor
   has_and_belongs_to_many :students
 
-  validates :instructor, presence: true
+  validates :instructor, :name, presence: true
 end
