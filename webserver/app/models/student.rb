53b1a91cc3e6520c4dcbc3380604d5bd8c092d27
Ryan Endacott <rzeg24@gmail.com>
2014-04-22 23:15:00 -0500

    both students and tas can have submissions

--- webserver/app/models/student.rb
+++ webserver/app/models/student.rb
@@ -1,4 +1,5 @@
 class Student < User
   has_and_belongs_to_many :courses
   has_and_belongs_to_many :sections
+  has_many :submissions, foreign_key: 'user_id'
 end
--- webserver/app/models/ta.rb
+++ webserver/app/models/ta.rb
@@ -1,3 +1,4 @@
 class Ta < User
   has_and_belongs_to_many :courses
+  has_many :submissions, foreign_key: 'user_id'
 end
