7fe995c38d63f55814c31b18b31becb86fdc71ad
Ryan Endacott <rzeg24@gmail.com>
2014-04-22 23:37:37 -0500

    assignments cannot have same name within course

--- webserver/app/models/assignment.rb
+++ webserver/app/models/assignment.rb
@@ -2,4 +2,7 @@ class Assignment < ActiveRecord::Base
   belongs_to :course
   has_many :submissions, dependent: :destroy
   validates :course, :name, :due_date, presence: true
+
+  # Courses may not have multiple assignments with the same name
+  validates :name, uniqueness: { scope: :course_id }
 end
