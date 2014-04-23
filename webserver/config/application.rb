5ec38e1a1d0983bab0767e26135396b8ad67c387
Ryan Endacott <rzeg24@gmail.com>
2014-04-23 01:14:29 -0500

    changed server time to central time

--- webserver/config/application.rb
+++ webserver/config/application.rb
@@ -11,11 +11,11 @@ module Webserver
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
+
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
-      
-      
+
+
       g.view_specs false
       g.helper_specs false
     end
@@ -26,7 +26,7 @@ module Webserver
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
