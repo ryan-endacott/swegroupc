8fec9f658fb36bc2a47f25a999e7cb8d38bb4a48
Ryan Endacott <rzeg24@gmail.com>
2014-04-22 23:15:10 -0500

    only show current user submissions

--- webserver/app/controllers/submissions_controller.rb
+++ webserver/app/controllers/submissions_controller.rb
@@ -11,7 +11,7 @@ class SubmissionsController < ApplicationController
   # GET /submissions
   # GET /submissions.json
   def index
-    @submissions = Submission.all
+    @submissions = current_user.submissions
   end
 
   # GET /submissions/1
