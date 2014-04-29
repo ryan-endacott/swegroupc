ff13351270022835becf8ee9a1157cb01ab564a6
Ryan Endacott <rzeg24@gmail.com>
2014-04-29 00:36:03 -0500

    Finally figured out how to use time zone correctly.  Finally!

--- webserver/app/controllers/assignments_controller.rb
+++ webserver/app/controllers/assignments_controller.rb
@@ -124,7 +124,7 @@ class AssignmentsController < ApplicationController
     def assignment_params
       p = params.require(:assignment).permit(:name, :due_date)
       if p[:due_date]
-        dd = Time.use_zone(Time.zone) { Time.strptime(p[:due_date], '%m/%d/%Y %H:%M %p') }
+        dd = ActiveSupport::TimeWithZone(nil, Time.zone, Time.strptime(p[:due_date], '%m/%d/%Y %H:%M %p'))
       end
       p[:due_date] = dd
       return p
